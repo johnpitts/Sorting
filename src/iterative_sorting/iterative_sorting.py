a956140dbe90fbfad5226124ba02d14e2df28429
johnpitts <jgpitts@gmail.com>
2020-01-21 19:10:06 -0500

    wrote BubbleSort & selectionSort and tested SAT.  wrote merge helper, but have a bug to clear before done

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,3 +1,10 @@
+
+def switch(smallest_index, i, arr):
+    temp = arr[smallest_index]
+    arr[smallest_index] = arr[i]
+    arr[i] = temp
+    return arr
+
 # TO-DO: Complete the selection_sort() function below 
 def selection_sort( arr ):
     # loop through n-1 elements
@@ -6,23 +13,38 @@ def selection_sort( arr ):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-             
-
-
+        for j in range(i, len(arr) - 1):
+            if arr[smallest_index] > arr[j+1]:
+                smallest_index = j+1
 
         # TO-DO: swap
-
-
-
-
+        if i != smallest_index:
+            # temp = arr[smallest_index]
+            # arr[smallest_index] = arr[i]
+            # arr[i] = temp
+            arr = switch(smallest_index, i, arr)
+        
     return arr
+testArray = [4, 3, 1, 2, 6, 5]
+print(selection_sort(testArray))
+
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-
+    notDone = True
+    while notDone:
+        # loop thru entire array even tho you should loop thru one less index each time
+        notDone = False
+        for index in range(0, len(arr) - 1):
+            if arr[index] > arr[index + 1]:
+                arr = switch(index + 1, index, arr)
+                notDone = True
     return arr
 
+
+testArray2 = [2, 3, 1, 4, 6, 5, 9, 7, 8, 10]
+print(bubble_sort(testArray2))
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -3,8 +3,30 @@ def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
     # TO-DO
-    
+    print(elements); print("\n")
+
+    index = 0; indexA = 0; indexB = 0
+    while index != (elements):
+
+        if arrA[indexA] <= arrB[indexB]:
+            nextSmallest = arrA[indexA]; print(f"A smaller: {nextSmallest}")
+            if indexA < len(arrA)-1:
+                indexA += 1
+            else:
+                arrA[indexA] = float("inf")
+        elif indexB != (len(arrB) - 1) :
+            nextSmallest = arrB[indexB]; print(nextSmallest, indexB)
+            if indexB < len(arrB)-1:
+                indexB += 1
+
+        merged_arr[index] = nextSmallest
+        index += 1
+
     return merged_arr
+
+array1 = [1, 4, 9, 11]
+array2 = [2, 7, 23, 49, 99]
+print(merge(array1, array2))
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
